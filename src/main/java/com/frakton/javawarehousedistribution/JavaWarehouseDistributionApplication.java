cc62e1e1160ab7f847168db9a457c0408321f308
RIlind Nuka <rilind.nuka@frakton.com>
2022-09-13 09:49:49 +0200

    Initial commit

---
+++ src/main/java/com/frakton/javawarehousedistribution/JavaWarehouseDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.frakton.javawarehousedistribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWarehouseDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWarehouseDistributionApplication.class, args);
+    }
+
+}
